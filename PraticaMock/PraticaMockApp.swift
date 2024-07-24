979ad5476d770c3d770ec8d61459dbddcd979976
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2024-07-24 11:10:01 -0300

    Initial Commit

---
+++ PraticaMock/PraticaMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PraticaMockApp.swift
+//  PraticaMock
+//
+//  Created by Lais Godinho on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PraticaMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
